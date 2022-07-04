90d7f68242207d77ff6b48568b7bc3b31a8f4651
Ricardo CPA <ricardo.devmob@gmail.com>
2022-07-04 15:50:59 -0300

    testando nullables

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,21 +1,5 @@
-import br.com.alura.bytebank.modelo.Endereco
-
 fun main() {
-    var enderecoNulo: Endereco? = Endereco(logradouro = "Avenida um", complemento = "casa")
-    val logradouroNovo: String? = enderecoNulo?.logradouro
-    enderecoNulo?.let {
-        println(it.logradouro.length)
-        val tamanhoComplemento: Int =
-            it.complemento?.length ?: throw IllegalStateException("Complemento não deve ser vazio")
-        println(tamanhoComplemento)
-    }
-    teste("")
-    teste(1)
+
 }
 
 
-
-fun teste(valor: Any){
-    val numero: Int? = valor as? Int
-    println(numero)
-}
---
+++ src/main/kotlin/br/com/alura/bytebank/teste/testaNullable.kt
@@ -0,0 +1,22 @@
+package br.com.alura.bytebank.teste
+
+import br.com.alura.bytebank.modelo.Endereco
+
+fun testaNullable() {
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Avenida um", complemento = "casa")
+    val logradouroNovo: String? = enderecoNulo?.logradouro
+    enderecoNulo?.let {
+        println(it.logradouro.length)
+        val tamanhoComplemento: Int =
+            it.complemento?.length ?: throw IllegalStateException("Complemento não deve ser vazio")
+        println(tamanhoComplemento)
+    }
+    teste("")
+    teste(1)
+}
+
+
+fun teste(valor: Any){
+    val numero: Int? = valor as? Int
+    println(numero)
+}
