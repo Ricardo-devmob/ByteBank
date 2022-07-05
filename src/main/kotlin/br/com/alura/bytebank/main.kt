689765375957cc6a1524b65c2820f824b24d4fee
Ricardo Ariel Costa Souza <ricardo.devmob@gmail.com>
2022-07-05 18:52:58 -0300

    Praticando programação funcional

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,5 +1,46 @@
-fun main() {
+import br.com.alura.bytebank.modelo.Autenticavel
+import br.com.alura.bytebank.modelo.Endereco
+import br.com.alura.bytebank.modelo.SistemaInterno
+import java.util.*
 
+fun main() {
+//    val endereco = Endereco(logradouro = "rua vergueiro", numero = 3185)
+//    val enderecoEmMaiusculo = "${endereco.logradouro}, ${endereco.numero}".uppercase(Locale.getDefault())
+//    println(enderecoEmMaiusculo)
+
+    val endereco = Endereco()
+        .also { println("Criando endereco") }
+        .apply {
+            logradouro = "rua vergueiro"
+            numero = 3185
+        }
+
+    with(endereco) {
+        "$logradouro, $numero".uppercase(Locale.getDefault())
+    }.let(::println)
+
+    listOf(
+        Endereco(complemento = "casa"),
+        Endereco(),
+        Endereco(complemento = "apartamento")
+    )
+        .filter(predicate = { endereco -> endereco.complemento.isNotEmpty() })
+        .let(block = (::println))
+
+    soma(1, 5, resultado = (::println))
+
+    val autenticavel = object : Autenticavel {
+        val senha = 1234
+        override fun autentica(senha: Int) = this.senha == senha
+    }
+
+    SistemaInterno().entra(autenticavel, 1234, autenticado = {
+        println("realizar pagamento")
+    })
 }
 
-
+fun soma(a: Int, b: Int, resultado: (Int) -> Unit) {
+    println("antes da soma")
+    resultado(a + b)
+    println("depois da soma")
+}
---
+++ src/main/kotlin/br/com/alura/bytebank/teste/testaTipoFuncao.kt
@@ -0,0 +1,54 @@
+package br.com.alura.bytebank.teste
+
+
+fun testaFuncaiAnonima() {
+    val minhaFuncaoAnonima = fun(a: Int, b: Int): Int {
+        return a + b
+    }
+    println(minhaFuncaoAnonima(20, 35))
+
+
+    val calculaBonificacaoAnonima: (salario: Double) -> Double = fun(salario): Double {
+        if (salario > 1000.0) {
+            return salario + 50
+        }
+        return salario + 100.0
+    }
+    println(calculaBonificacaoAnonima(1200.0))
+}
+
+fun testaFuncaoLambda() {
+    val minhaFuncaoLambda = { a: Int, b: Int ->
+        a + b
+    }
+    println(minhaFuncaoLambda(15, 10))
+    val calculaBonificacao: (salario: Double) -> Double = lambda@{ salario ->
+        if (salario > 1000.0) {
+            return@lambda salario + 50
+        }
+        salario + 100.0
+    }
+    println(calculaBonificacao(1100.0))
+}
+
+fun testaTipoFuncaoClasse() {
+    val minhaFuncaoClasse: (Int, Int) -> Int = Soma()
+    println(minhaFuncaoClasse(10, 10))
+}
+
+fun testeTipoFuncaoReferencia() {
+    val minhaFuncao = ::soma
+    println(minhaFuncao(5, 10))
+}
+
+fun soma(a: Int, b: Int) : Int {
+    return a + b
+}
+
+fun teste() {
+    println("executa teste")
+}
+
+class Soma : (Int, Int) -> Int {
+    override fun invoke(a: Int, b: Int): Int = a + b
+}
